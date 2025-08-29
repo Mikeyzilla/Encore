669a0ad11107920a00d6c08075be5c641cd00023
Michael Casella <mjc52618@uga.edu>
2025-08-28 22:33:23 -0400

    Working on styling band profile view. Continuing work from bandorigin onwards.

--- Encore-Frontend/src/components/CreateAccount/CreateAccount.tsx
+++ Encore-Frontend/src/components/CreateAccount/CreateAccount.tsx
@@ -3,7 +3,8 @@ import { useSwipeable } from "react-swipeable";
 import "./CreateAccount.css";
 import bandProfileImage from "../../assets/RockProfile.png";
 import managerBackground from "../../assets/ManagerCreate.png";
-
+import { musicMap, musicProfileMap } from "../../utils/BandGenres";
+import type { musicGenres } from "../../utils/BandGenres";
 type UIType = "normal" | "band" | "manager";
 
 export default function CreateAccount() {
@@ -16,11 +17,20 @@ export default function CreateAccount() {
   const [password, setPassword] = useState<string>("");
   const [nameOfTheBand, setNameOfTheBand] = useState<string>("");
   const [whatIsShown, setWhatIsShown] = useState<UIType>("normal");
+  const [genre, setGenre] = useState<musicGenres>("rock");
   const [isUserShowing, setIsUserShowing] = useState(true);
+  const [bandName, setBandName] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [mostPlayedSong, setMostPlayedSong] = useState("");
+  const [aboutUs, setAboutUs] = useState("");
+  const [newBandSongs, setNewBandSongs] = useState<string[]>([]);
+  const [pendingSong, setPendingSong] = useState("");
+  const [newBand, setNewBand] = useState(false)
   const [nameOfInformationArea, setNameOfInformationArea] = useState("RequiredInformationArea SwipeZone");
   const showBand = () => setWhatIsShown("band");
   const showManager = () => setWhatIsShown("manager");
   const showNormal = () => setWhatIsShown("normal");
+  const genreKey = (genre?.toLowerCase().trim() as musicGenres) || "rock";
 
   const showVenueEntries = () => {
     setIsUserShowing(false);
@@ -46,7 +56,7 @@ export default function CreateAccount() {
     <div
       className="AccountCreationPage"
       style={{
-        backgroundImage: `url(${whatIsShown === "band" ? bandProfileImage : managerBackground})`,
+        backgroundImage: `url(${whatIsShown === "band" ? musicProfileMap[genreKey].backgroundImage : managerBackground})`,
       }}
     >
       {whatIsShown === "normal" && (
@@ -79,47 +89,140 @@ export default function CreateAccount() {
       )}
 
       {whatIsShown === "band" && (
-        <div>
-          <div onClick={showNormal} style={{ cursor: "pointer" }}>← Back</div>
-          <h1 className="NameOfBrand">Encore</h1>
-          <input type="text" placeholder="Your username goes here"></input>
-          <input type="text" placeholder="Please enter the name of your band"></input>
-          <input type="text" placeholder="Your password goes here"></input>
-          <input type="text" placeholder="Here's an opportunity to tell managers a little about you"></input>
+        <div className="BandView">
+          <div className="FillableTitleArea">
+              <select className="GenreSelector" value={genre} onChange={(e) => setGenre(e.target.value as musicGenres)}>
+                <option value="rock">rock</option>
+                <option value="punk">punk</option>
+                <option value="pop">pop</option>
+                <option value="grunge">grunge</option>
+                <option value="metal">metal</option>
+                <option value="country">country</option>
+                <option value="alternative">alternative</option>
+                <option value="gospel">gospel</option>
+                <option value="latin">latin</option>
+              </select>
+            <h1 className="NameOfBrandTitle">Encore</h1>
+            <div className="BackClick" onClick={showNormal} style={{ cursor: "pointer" }}>← Back</div>
+          </div>
+          <div className="LayoutChangeArea">
+            <p className="UsefulTidBit">If you don't have any albums or past performances, no worries! Click here to change the layout.</p>
+            <input type="checkbox" className="NewBandCheckbox" onChange={(e) => setNewBand(e.target.checked)} />
+          </div>
+          <div
+            className="fillOutableBandProfile"
+            style={{
+              fontFamily: musicProfileMap[genreKey].fontFamily,
+              color: musicProfileMap[genreKey].color,
+            }}
+          >
+            <div className="TopOfBandArea">
+              <input type="text" placeholder="Name of your Band" className="BandNameGoesHere"></input>
+              <p className="BandOrigin">{origin}</p>
+              <div className="LineOfSocials">
+                <div className="SocialInfo">
+                  <div className="SocialImage"></div>
+                  <span>Social Media Brand</span>
+                </div>
+                <div className="HitSongPart">
+                  <div className="SongLogo"></div>
+                  <span>{mostPlayedSong}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="MiddleOfTheProfile">
+              {newBand && (
+                <div className="EarlyBandSection">
+                  <div className="SongLand">
+                    <h1 className="SongsSoFarTitle">Our Songs so far:</h1>
+                    <input type="text"></input>
+                  </div>
+                  <div className="DescriptionLand">
+                    <h1 className="AboutUsLand">A little about us</h1>
+                    <span className="AboutDescriptions">{aboutUs}</span>
+                  </div>
+                </div>
+              )}
+              {!newBand && (
+                <div className="ReturningLand">
+                  <div className="DescriptionOfWe">
+                    {aboutUs}
+                  </div>
+                  <div className="Glue">
+                    <div className="PerformancesLand">
+                      <div className="PerformanceAddition">
+                        <div className="PictureOfTour"></div>
+                        <div className="PerformanceInformationLand">
+                          <input type="text" placeholder="Where did you play?"></input>
+                          <input type="text" placeholder="When did you play?"></input>
+                          <input type="text" placeholder="Describe how the event went"></input>
+                          <input type="text" placeholder="How many people showed up?"></input>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="AlbumsLand">
+                    <div className="AlbumCoverImage">
+                      <input type="text" placeholder="What was the name of your album?"></input>
+                    </div>
+                    <div className="AlbumInformationLand">
+                      <input type="text" placeholder="Where did your album rank on the charts?"></input>
+                      <input type="text" placeholder="How much money did you make off your album?"></input>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {newBand && (
+                <div className="NewUserBottom">
+                  <div className="FutureGoalsLand">
+                    <div className="PhotoOfTheGroup"></div>
+                    <div className="DescriptorAreaLand">
+                      <h1 className="GoalsOfWe">Our Goals</h1>
+                      <input type="text" placeholder="Give us a brief description of your future goals as a band"></input>
+                      <input type="text" placeholder="Why should a manager choose to hire you?"></input>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       )}
 
-      {whatIsShown === "manager" && (
-        <div className="ManagerArea">
-          <div className="ManagerHeader">
-            <h1 className="ManagerNameOfBrand">Encore VIP</h1>
-            <div className="BackBtn" onClick={showNormal} style={{ cursor: "pointer" }}>← Back</div>
+      {
+        whatIsShown === "manager" && (
+          <div className="ManagerArea">
+            <div className="ManagerHeader">
+              <h1 className="ManagerNameOfBrand">Encore VIP</h1>
+              <div className="BackBtn" onClick={showNormal} style={{ cursor: "pointer" }}>← Back</div>
+            </div>
+            <div className="ManagerHeader">
+              <h2 className="ManagerInfo">Managers, you now have the opportunity to enter in all venue information for the shows you want to have filled!</h2>
+            </div>
+            <h4 className="TidBit">Don't worry if you don't get to it - you'll have the opportunity to update them through our Management Dashboard.</h4>
+            <h5>Swipe left to add your venue information. Swipe right to go back to the username view.</h5>
+            <div className={nameOfInformationArea} {...entryViewHandler}>
+              {isUserShowing && (
+                <div className="AccountView">
+                  <input className="ManagerName" type="text" placeholder="Your username goes here" required></input>
+                  <input className="ManagerPass" type="password" placeholder="Your password goes here" required></input>
+                </div>
+              )}
+              {!isUserShowing && (
+                <div className="VenueView">
+                  <input className="VenueEntry" type="text" placeholder="What type of event is it?"></input>
+                  <input className="VenueEntry" type="text" placeholder="Where is the venue?"></input>
+                  <input className="VenueEntry" type="text" placeholder="What timeslot is it for?"></input>
+                  <input className="VenueEntry" type="text" placeholder="What date is it for?"></input>
+                  <input className="VenueEntry" type="text" placeholder="What's the band's fee for the gig?"></input>
+                </div>
+              )}
+              <input type="submit" className="ManagerSubmit"></input>
+            </div>
           </div>
-          <div className="ManagerHeader">
-            <h2 className="ManagerInfo">Managers, you now have the opportunity to enter in all venue information for the shows you want to have filled!</h2>
-          </div>
-          <h4 className="TidBit">Don't worry if you don't get to it - you'll have the opporunity to update them through our Management Dashboard.</h4>
-          <h5>Swipe left to add your venue information. Swipe right to go back to the username view.</h5>
-          <div className={nameOfInformationArea} {...entryViewHandler}>
-            {isUserShowing && (
-              <div className="AccountView">
-                <input className="ManagerName" type="text" placeholder="Your username goes here" required></input>
-                <input className="ManagerPass" type="password" placeholder="Your password goes here" required></input>
-              </div>
-            )}
-            {!isUserShowing && (
-              <div className="VenueView">
-                <input className="VenueEntry" type="text" placeholder="What type of event is it?"></input>
-                <input className="VenueEntry" type="text" placeholder="Where is the venue?"></input>
-                <input className="VenueEntry" type="text" placeholder="What timeslot is it for?"></input>
-                <input className="VenueEntry" type="text" placeholder="What date is it for?"></input>
-                <input className="VenueEntry" type="text" placeholder="What's the band's fee for the gig?"></input>
-              </div>
-            )}
-            <input type="submit" className="ManagerSubmit"></input>
-          </div>
-        </div>
-      )}
-    </div>
+        )
+      }
+    </div >
   );
 }
--- ToDo.md
+++ ToDo.md
@@ -1,26 +1,27 @@
 CURRENT GOAL: Implement account creation and role based auth.
-                A) Add in necessary Band UI fields and styles (styles switch on genre selection - use musicMap[genrekey]) to make it feel like you're building your band profile. 
-                B) Fix the position of the group photo, and generate one per genre type.
-                C) Populate DB with Data. (Make all bands that have performances have albums, all bands that have albums have performances, and all bands that don't have either have the elevator pitch and why choose us filled out)
-                D) Bring the Our Goals section on BandProfile higher up.
-                E) Push the performance information section slightly to the right.
-                F) Make the Ranked + Generated Album data's font sizes larger.
-                G) Add in a tour picture and album cover per band.
-                H) Have the spotify section expand for long song titles.
-                I) Make the songs in our songs so far larger in font size, as well as aligned better.
-                J) Try to make font colors consistent from genre -> band sneak -> band profile.
-                K) Make a login page on the frontend.
-                L) Create a users table in the DB.
-                M) Create a users entity in the backend.
-                N) Create a user repo in the backend.
-                O) Create a user-centric controller that features both account creation and login.
-                P) Hook those services up to the frontend pages.
-                Q) Protect routes using role-based auth. In app.routes, make sure only managers can use the three routes that currently exist which are not account creation nor login by the use of JWTs.
-                R) Test it out.
-                S) Once a Manager logs in, they should be taken to the genre view.
-                T) Make account create the default route (otherwise known as / )
-                U) Once a Band logs in, take them to their profile page.
-                V) Move on to Band Calendar view.
+                A) Style the band profile view inside of account creation. (Start from bandorigin now).
+                B) use value + onChange for inputs inside of Band Account Creation.
+                C) Add a way to add / remove songs in Band Account Creation.
+                D) Fix the position of the group photo, and generate one per genre type.
+                E) Bring the Our Goals section on BandProfile higher up.
+                F) Push the performance information section slightly to the right.
+                G) Make the Ranked + Generated Album data's font sizes larger.
+                H) Add in a tour picture and album cover per band.
+                I) Have the spotify section expand for long song titles.
+                J) Make the songs in our songs so far larger in font size, as well as aligned better.
+                K) Try to make font colors consistent from genre -> band sneak -> band profile.
+                L) Make a login page on the frontend.
+                M) Create a users table in the DB.
+                N) Create a users entity in the backend.
+                O) Create a user repo in the backend.
+                P) Create a user-centric controller that features both account creation and login.
+                Q) Hook those services up to the frontend pages.
+                R) Protect routes using role-based auth. In app.routes, make sure only managers can use the three routes that currently exist which are not account creation nor login by the use of JWTs.
+                S) Test it out.
+                T) Once a Manager logs in, they should be taken to the genre view.
+                U) Make account create the default route (otherwise known as / )
+                V) Once a Band logs in, take them to their profile page.
+                W) Move on to Band Calendar view.
 
 Future Goals:  
 A) Change the JPA repository methods to sort by most recent / latest / highest grossing.
